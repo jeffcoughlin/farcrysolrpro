3029aaa3378eef4489f98c69fdaf8e09e8a6f10d
Jeff Coughlin <jeff@jeffcoughlin.com>
2012-03-20 10:08:06 -0400

    refs #22: Allow bash/batch files to be run from anywhere on the server

--- packages/custom/cfsolrlib/solr-server/start.bat
+++ packages/custom/cfsolrlib/solr-server/start.bat
@@ -7,6 +7,7 @@ set XMS="256M"
 set XMX="512M"
 
 rem Folder location for solr-server (must end in a slash)
+rem set SOLRDIR="C:\program files (x86)\solr-server\"
 set SOLRDIR="./"
 
 rem Set unique port and stop key
@@ -25,4 +26,5 @@ echo FarCry Solr Pro plugin
 echo =======================
 echo Starting Solr Server...
 
+cd %SOLRDIR%
 java -Dsolr.solr.home=multicore -DSTOP.PORT=%DTSOPPORT% -DSTOP.KEY=%DSTOPKEY% -Xms%XMS%  -Xmx%XMX% -jar %SOLRDIR%start.jar
--- packages/custom/cfsolrlib/solr-server/stop.bat
+++ packages/custom/cfsolrlib/solr-server/stop.bat
@@ -3,6 +3,7 @@ rem description: Stops the Solr server for the FarCry Solr Pro plugin
 rem author: Jeff Coughlin (www.jeffcoughlin.com), Sean Coyne (www.n42designs.com)
 
 rem Folder location for solr-server (must end in a slash)
+rem set SOLRDIR="C:\program files (x86)\solr-server\"
 set SOLRDIR="./"
 
 rem Set unique port and stop key
@@ -18,5 +19,5 @@ echo FarCry Solr Pro plugin
 echo =======================
 echo Stopping Solr Server...
 
-
+cd %SOLRDIR%
 java -Dsolr.solr.home=multicore -DSTOP.PORT=%DTSOPPORT% -DSTOP.KEY=%DSTOPKEY% -jar %SOLRDIR%start.jar --stop
