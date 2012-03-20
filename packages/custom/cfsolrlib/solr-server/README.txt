d139e64b893c18b4d6fa3a7155735c78a0e8a1a0
Jeff Coughlin <jeff@jeffcoughlin.com>
2012-03-20 09:25:51 -0400

    fixes #22: Start/stop helper scripts

--- packages/custom/cfsolrlib/solr-server/README.txt
+++ packages/custom/cfsolrlib/solr-server/README.txt
@@ -7,3 +7,5 @@ To run the Solr instance that ships with the plugin run the following:
 in this directory, and when Solr is started connect to 
 
   http://localhost:8983/solr/admin/
+
+For assistance you may wish to run the executable bash script (for *nix/Mac) or the start.bat/stop.bat files (for Windows).  Just note that the sample scripts use a min/max RAM settings of 256/512 respectively.  You will likely want to increase those for production use (just edit the files as desired or create your own elsewhere on your server using these as starting templates).
---
+++ packages/custom/cfsolrlib/solr-server/stop.bat
@@ -0,0 +1,22 @@
+@echo off
+rem description: Stops the Solr server for the FarCry Solr Pro plugin
+rem author: Jeff Coughlin (www.jeffcoughlin.com), Sean Coyne (www.n42designs.com)
+
+rem Folder location for solr-server (must end in a slash)
+set SOLRDIR="./"
+
+rem Set unique port and stop key
+set DTSOPPORT="8079"
+set DSTOPKEY="farcrysolrpro"
+
+rem ###
+rem Edit settings/variables above this line as needed
+
+echo.
+echo =======================
+echo FarCry Solr Pro plugin
+echo =======================
+echo Stopping Solr Server...
+
+
+java -Dsolr.solr.home=multicore -DSTOP.PORT=%DTSOPPORT% -DSTOP.KEY=%DSTOPKEY% -jar %SOLRDIR%start.jar --stop
